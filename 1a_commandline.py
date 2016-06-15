72edc3ac50f0199b03fa00e0b330fbd04ac01953
adasgupta98 <anushkad@princeton.edu>
2016-06-15 08:48:31 -0400

    Update and rename 1a100x2.py to 1a_commandline.py
    Add comments, remove lines that are unnecessary when running from command line, change increments at which c_var and free energy data is saved

--- 1a_commandline.py
+++ 1a_commandline.py
@@ -13,6 +13,11 @@ c, rho_s, c_alpha, c_beta = sympy.symbols("c_var rho_s c_alpha c_beta")
 f_0 = rho_s * (c - c_alpha)**2 * (c_beta - c)**2
 
 sympy.diff(f_0, c, 2)
+
+# command format:
+# python 1a_commandline.py 100 2.0
+# where nx = 100
+# and dx = 2.0
 nx = int(sys.argv[1])
 dx = float(sys.argv[2])
 
@@ -26,21 +31,16 @@ c_0 = 0.5
 epsilon = 0.01
 rho_s = 5.0
 
+# solution variable
 c_var = fp.CellVariable(mesh=mesh, name=r"$c$", hasOld=True)
 
 # array of sample c-values: used in f versus c plot
 vals = np.linspace(-.1, 1.1, 1000)
 
-c_var = fp.CellVariable(mesh=mesh, name=r"$c$", hasOld=True)
-
 x , y = np.array(mesh.x), np.array(mesh.y)
 
-c_var[:] = c_0 + epsilon * (np.cos(0.105 * x) * np.cos(0.11 * y) +                             (np.cos(0.13 * x) * np.cos(0.087 * y))**2 +                             + np.cos(0.025 * x - 0.15 * y) * np.cos(0.07 * x - 0.02 * y))
-
-out = sympy.diff(f_0, c, 2)
-exec "f_0_var = " + repr(out)
-#f_0_var = -A + 3*B*(c_var - c_m)**2 + 3*c_alpha*(c_var - c_alpha)**2 + 3*c_beta*(c_var - c_beta)**2
-f_0_var
+# initial value for square and T domains
+c_var[:] = c_0 + epsilon * (np.cos(0.105 * x) * np.cos(0.11 * y) + (np.cos(0.13 * x) * np.cos(0.087 * y))**2 + np.cos(0.025 * x - 0.15 * y) * np.cos(0.07 * x - 0.02 * y))
 
 # bulk free energy density
 def f_0(c):
@@ -68,20 +68,20 @@ def save_data(f, time):
     f_data.append(f.value)
     time_data.append(time)
     np.savetxt(file_name, zip(time_data, f_data))
-    
+
+# solver equation    
 eqn = fp.TransientTerm(coeff=1.) == fp.DiffusionTerm(M * f_0_var(c_var)) - fp.DiffusionTerm((M, kappa))
-eqn
 
 elapsed = 0.0
 steps = 0
 dt = 0.01
 total_sweeps = 2
 tolerance = 1e-1
+
+# controls on how long the simulation runs: steps, duration, or both
 total_steps = 4000
 duration = 3000.0
-viewers = []
 
-c_var[:] = c_0 + epsilon * (np.cos(0.105 * x) * np.cos(0.11 * y) +                             (np.cos(0.13 * x) * np.cos(0.087 * y))**2 +                             + np.cos(0.025 * x - 0.15 * y) * np.cos(0.07 * x - 0.02 * y))
 c_var.updateOld()
 from fipy.solvers.pysparse import LinearLUSolver as Solver
 solver = Solver()
@@ -93,17 +93,25 @@ while steps <= total_steps:
         res = eqn.sweep(c_var, dt=dt, solver=solver)
 
     if res < res0 * tolerance:
-        # record the volume integral of the free energy 
-        save_data(f(c_var).cellVolumeAverage*mesh.numberOfCells, elapsed)
-        # pickle the data on c as a function of space at this particular time
-        file_dir = "1apickles{0}x{1}".format(nx, dx)
+        # checks whether a folder for the pickles from this simulation exists
+        # if not, creates one in the home directory
+        file_dir = "~/1apickles{0}x{1}".format(nx, dx)
         if not os.path.isdir(file_dir):
             os.makedirs(file_dir)
-        fp.dump.write({'time' : steps, 'var': c_var}, '1apickles{0}x{1}/1a_{2}.pkl'.format(nx, dx, steps))
         
-        if (steps%(total_steps/10)==0):
+        # anything in this loop will only be executed 100 times
+        if (steps%(total_steps/100)==0):
             print steps
             print elapsed
+            
+            # record the volume integral of the free energy 
+            # equivalent to the average value of the free energy for any cell,
+            # multiplied by the number of cells and the area of each cell
+            # (since this is a 2D domain)
+            save_data(f(c_var).cellVolumeAverage*mesh.numberOfCells*(dx**2), elapsed)
+            # saves c_var in pickle file
+            fp.dump.write({'time' : steps, 'var': c_var}, '1apickles{0}x{1}/1a_{2}.pkl'.format(nx, dx, steps))
+            
             
         steps += 1
         elapsed += dt
@@ -113,6 +121,6 @@ while steps <= total_steps:
         dt *= 0.8
         c_var[:] = c_var.old
 
-viewer.plot()
+# simulation ends
 print 'elapsed_time:', elapsed
 
